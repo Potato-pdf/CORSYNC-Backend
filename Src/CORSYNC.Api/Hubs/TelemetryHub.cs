e8a40578ce25e0d4ce19c8e8eaf83ca402351afe
Potato-pdf <175372743+Potato-pdf@users.noreply.github.com>
2026-06-22 22:39:11 -0600

    chore: add TelemetryHub.cs file to project structure

---
+++ Src/CORSYNC.Api/Hubs/TelemetryHub.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.SignalR;
+
+namespace CORSYNC.Api.Hubs
+{
+    public class TelemetryHub : Hub
+    {
+        // Clients connect to this Hub to receive live telemetry broadcasts
+    }
+}
