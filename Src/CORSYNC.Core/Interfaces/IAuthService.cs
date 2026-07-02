0f9ab941d8d5f1e87226bf1844e6f444c3c65de9
Potato-pdf <175372743+Potato-pdf@users.noreply.github.com>
2026-07-01 21:59:45 -0600

    Actualiza Src/CORSYNC.Core/Interfaces/IAuthService.cs

--- Src/CORSYNC.Core/Interfaces/IAuthService.cs
+++ Src/CORSYNC.Core/Interfaces/IAuthService.cs
@@ -1,3 +1,4 @@
+using System.Security.Claims;
 using CORSYNC.Core.Domain;
 
 namespace CORSYNC.Core.Interfaces
@@ -7,5 +8,7 @@ namespace CORSYNC.Core.Interfaces
         string HashPassword(string password);
         bool VerifyPassword(string password, string passwordHash);
         string GenerateJwtToken(Usuario user);
+        RefreshToken GenerateRefreshToken(int usuarioId);
+        ClaimsPrincipal? GetPrincipalFromExpiredToken(string token);
     }
 }
